cd548c482dc3d579aa0a2ea8e6221c9bf871d425
Mark Smith <mark@markjs.net>
2012-02-07 11:45:13 +0000

    end of chapter f - ajax fun

--- app/controllers/line_items_controller.rb
+++ app/controllers/line_items_controller.rb
@@ -46,7 +46,8 @@ class LineItemsController < ApplicationController
   
     respond_to do |format|
       if @line_item.save
-        format.html { redirect_to @line_item.cart }
+        format.html { redirect_to store_url }
+        format.js   { @current_item = @line_item }
         format.json { render json: @line_item, status: :created, location: @line_item }
       else
         format.html { render action: "new" }
@@ -78,7 +79,7 @@ class LineItemsController < ApplicationController
     @line_item.destroy
 
     respond_to do |format|
-      format.html { redirect_to @line_item.cart }
+      format.html { redirect_to store_url }
       format.json { head :ok }
     end
   end
